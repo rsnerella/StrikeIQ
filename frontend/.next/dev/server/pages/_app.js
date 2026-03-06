a7dae4f6751d26eb1262392aea6277e48bb3f62e
Mohit Kulkarni <Kulkarnimohit1999@gmail.com>
2026-03-06 13:42:08 +0530

    chore: add .next/ to .gitignore and remove from tracking

--- frontend/.next/dev/server/pages/_app.js
+++
@@ -1,6 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/src_components_layout_Navbar_tsx_b1d0d2b7._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__e035543c._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__d0d82bbd._.js")
-R.m("[project]/src/pages/_app.tsx [ssr] (ecmascript)")
-module.exports=R.m("[project]/src/pages/_app.tsx [ssr] (ecmascript)").exports
--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vU/9FQlmdLfG50RWFqLNWwroUoQ3kNqVwybsEafaETs=\"\n}"
